eb8c411f79b3e16d6edbadfd0d9c257fcdd599b2
calgor <ahomayoni684@gmail.com>
2023-04-06 14:05:20 +0330

    added NewCache Func

--- database/db.go
+++ database/db.go
@@ -14,3 +14,12 @@ type value struct {
 	value          interface{}
 	expirationDate time.Time
 }
+
+func NewCache() *Cache {
+	cache := &Cache{data: make(map[string]value)}
+	go cache.startCleanup()
+}
+
+func (c *Cache) startCleanup() {
+
+}
