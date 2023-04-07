84053bcb00e1c28b219656b9ed9c592967e89d1e
calgor <ahomayoni684@gmail.com>
2023-04-07 11:36:30 +0330

    added TTL deletion

--- database/db.go
+++ database/db.go
@@ -18,8 +18,19 @@ type value struct {
 func NewCache() *Cache {
 	cache := &Cache{data: make(map[string]value)}
 	go cache.startCleanup()
+	return cache
 }
 
 func (c *Cache) startCleanup() {
-
+	for {
+		time.Sleep(time.Minute)
+		c.Lock()
+		now := time.Now()
+		for k, v := range c.data {
+			if v.expirationDate.IsZero() && now.Compare(v.expirationDate) == 1 {
+				delete(c.data, k)
+			}
+		}
+		c.Unlock()
+	}
 }
