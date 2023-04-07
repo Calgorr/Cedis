b65f425f12522b39532bc17098ff20b5435ad0da
calgor <ahomayoni684@gmail.com>
2023-04-07 12:22:39 +0330

    concurrent access test added

--- database/db_test.go
+++ database/db_test.go
@@ -1,6 +1,8 @@
 package database_test
 
 import (
+	"fmt"
+	"sync"
 	"testing"
 	"time"
 
@@ -59,5 +61,48 @@ func TestCache_Cleanup(t *testing.T) {
 
 	if exists {
 		t.Errorf("Expected 'foo' to not exist in cache after cleanup")
+	}
+}
+
+func TestCache_ConcurrentAccess(t *testing.T) {
+	c := database.NewCache()
+	var wg sync.WaitGroup
+
+	// set 1000 keys with a TTL of 100ms concurrently
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			c.Set(fmt.Sprint(i), "value", time.Millisecond*100)
+		}(i)
+	}
+
+	// wait for all keys to be set
+	wg.Wait()
+
+	// access all keys concurrently
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			_, exists := c.Get(fmt.Sprint(i))
+			if !exists {
+				t.Errorf("Expected key %v to exist in cache", i)
+			}
+		}(i)
+	}
+
+	// wait for all keys to be accessed
+	wg.Wait()
+
+	// wait for all keys to expire
+	time.Sleep(time.Millisecond * 200)
+
+	// ensure all keys have been removed
+	for i := 0; i < 1000; i++ {
+		_, exists := c.Get(fmt.Sprint(i))
+		if exists {
+			t.Errorf("Expected key %v to not exist in cache", i)
+		}
 	}
 }
