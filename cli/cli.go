f07cf744e08ff81dac63d00459b122f10d8d4501
calgor <ahomayoni684@gmail.com>
2023-04-08 17:02:23 +0330

    added new parser

--- cli/cli.go
+++ cli/cli.go
@@ -2,6 +2,7 @@ package cli
 
 import (
 	"bufio"
+	"os"
 
 	"github.com/Calgorr/Cedis/container"
 )
@@ -9,4 +10,8 @@ import (
 type Parser struct {
 	redis  *container.Container
 	reader *bufio.Reader
+}
+
+func NewParser(c *container.Container) *Parser {
+	return &Parser{redis: c, reader: bufio.NewReader(os.Stdin)}
 }
