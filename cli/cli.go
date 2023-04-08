4e213ce14fe990fc4179912a0036a461a996c76e
calgor <ahomayoni684@gmail.com>
2023-04-08 17:25:29 +0330

    added programming loop

--- cli/cli.go
+++ cli/cli.go
@@ -3,6 +3,7 @@ package cli
 import (
 	"bufio"
 	"os"
+	"strings"
 
 	"github.com/Calgorr/Cedis/container"
 )
@@ -14,4 +15,19 @@ type Parser struct {
 
 func NewParser(c *container.Container) *Parser {
 	return &Parser{redis: c, reader: bufio.NewReader(os.Stdin)}
+}
+
+func (p *Parser) StartProgrammingLoop() error {
+	for {
+		input, err := p.reader.ReadString('\n')
+		if err != nil {
+			return err
+		}
+		p.parse(strings.TrimSpace(input))
+	}
+	return nil
+}
+
+func (p *Parser) parse(input string) error {
+
 }
