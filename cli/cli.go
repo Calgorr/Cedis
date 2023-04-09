a0059883e716a7a1106e17ba17aeaef9d075d812
calgor <ahomayoni684@gmail.com>
2023-04-09 12:00:06 +0330

    added get command

--- cli/cli.go
+++ cli/cli.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 
@@ -41,5 +42,44 @@ func (p *Parser) parse(input string) error {
 		key, value, Duration := strings.Split(input, "")[0], strings.Split(input, "")[1], strings.Split(input, "")[2]
 		ttl, _ := time.ParseDuration(Duration)
 		p.redis.CurrentDatabase.Set(key, value, ttl)
+	case "get":
+		if p.redis.CurrentDatabase == nil {
+			return errors.New("No Database selected")
+		}
+		key := strings.Split(input, " ")[0]
+		value, ok := p.redis.CurrentDatabase.Get(key)
+		if !ok {
+			return errors.New("Key does not exist")
+		}
+		fmt.Println(value)
+	case "del":
+		if p.redis.CurrentDatabase == nil {
+			return errors.New("No Database selected")
+		}
+		key := strings.Split(input, " ")[0]
+		fmt.Println(p.redis.CurrentDatabase.Delete(key))
+	case "keys":
+		if p.redis.CurrentDatabase == nil {
+			return errors.New("No Database selected")
+		}
+		pattern := strings.Split(input, " ")[1]
+		keys, err := p.redis.CurrentDatabase.KeysMatchesPatern(pattern)
+		if err != nil {
+			return err
+		}
+		fmt.Println(keys)
+	case "select":
+		if p.redis.CurrentDatabase == nil {
+			return errors.New("No Database selected")
+		}
+		db, _ := strconv.Atoi(strings.Split(input, " ")[1])
+		p.redis.CurrentDatabase = p.redis.GetAllDatabases()[db]
+
+	case "exit":
+		os.Exit(0)
+	default:
+		return errors.New("invalid command")
 	}
+	return nil
+
 }
